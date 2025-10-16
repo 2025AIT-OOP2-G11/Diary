5116fe7c3f30286dceab60eac160f8313a79b288
Shimoda443 <shijimi0811@icloud.com>
2025-10-16 15:43:30 +0900

    下田の日記
    日記追加しました

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from diaries.DiarySample import DiarySample
+from diaries.ShimodaDiary import ShimodaDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [DiarySample(), ]
+for d in diaries:
+    print("---------------------------------")
+    print(d.get_date())
+    print(d.get_summary())
+    print(d.get_author())
+    print()
+
+diaries = [ShimodaDiary(), ]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
