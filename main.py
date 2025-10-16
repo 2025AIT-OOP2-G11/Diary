e4ee0f9321d2a6ed84ff9edd6543d56a66446bc4
nrak126 <daichidaichi126@gmail.com>
2025-10-16 15:53:33 +0900

    全角スペースを削除

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from diaries.ShimodaDiary import ShimodaDiary
 from diaries.IkegamiDiary import IkegamiDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), YabeDiary(), YonekuraDiary(), ShimodaDiary(), IkegamiDiary(),　]
+diaries = [DiarySample(), YabeDiary(), YonekuraDiary(), ShimodaDiary(), IkegamiDiary(), ]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
